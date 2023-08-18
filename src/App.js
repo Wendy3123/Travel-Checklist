53c2fed5230b0f02f95fdabc416fb809dae79bd1
Wendy Wang <wendywangg928@gmail.com>
2023-08-17 21:07:38 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
 import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 5, packed: true },
+];
 
 export default function App() {
   return (
@@ -16,37 +23,75 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return; //if there is no description return nothing
+
+    const newItem = { description, quantity, packed: false, id: Date.now };
+
+    setDescription(""); //After the submit button set the description back to an empty string
+    setQuantity(1); //also set the quantity back to the default 1 item
+  }
+
   return (
-    <div>
-      <form className="add-form">
-        <h3>What do you need for your 😍 trip?</h3>
-        <label for="numberItem"></label>
-        <select id="numberItem" name="numberItem">
-          <option value="one">1</option>
-          <option value="two">2</option>
-          <option value="three">3</option>
-          <option value="four">4</option>
-          <option value="five">5</option>
-          <option value="six">6</option>
-        </select>
-        <label for="itemName"></label>
-        <input
-          type="text"
-          id="itemName"
-          name="itemName"
-          placeholder="Item..."
-        ></input>
-        <button className="button">Add</button>
-      </form>
-    </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <label for="numberItem"></label>
+      {/* the array below has a length of 20 creating an empty array with 20
+      elements, as the second part it passes a map function (the first part is
+      the _ which is the current value and second part is the index i.) Then we
+      return the index which is 0 +1. so the array is from 1-20. Finally add the .map() like regular */}
+      <select
+        id="numberItem"
+        name="numberItem"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <label for="itemName"></label>
+      <input
+        type="text"
+        value={description}
+        placeholder="Item..."
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list"></div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-function Item() {}
+function Item({ item }) {
+  return (
+    //the first span style tag says if the item.packed = true we will add a css property strikethough to cross out the item
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 
 function Stats() {
   return (
